e410c110d2061b5a418e1234d62cb77c10fa5b44
Anh Tran <webfullstack99@gmail.com>
2026-04-12 18:23:20 +0700

    update vite base

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()],
-  // test: {
-  //   environment: "jsdom",
-  //   setupFiles: "./src/tests/setup.ts",
-  // },
+export default defineConfig(({ command }) => {
+  const isBuild = command === "build";
+
+  return {
+    plugins: [react()],
+    base: isBuild ? "/badguys/" : "/",
+  };
 });
