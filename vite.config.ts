c0350212f64a1a4d88b031eaa0a1ff5177335dd1
Anh Tran <webfullstack99@gmail.com>
2026-04-12 18:20:19 +0700

    disable test for now

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  test: {
-    environment: "jsdom",
-    setupFiles: "./src/tests/setup.ts",
-  },
+  // test: {
+  //   environment: "jsdom",
+  //   setupFiles: "./src/tests/setup.ts",
+  // },
 });
